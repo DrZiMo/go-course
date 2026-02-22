2ea55d8582ea83fa40922b42a85f68be75f91322
DrZiMo <zuhaybhamar@gmail.com>
2026-02-22 06:51:55 +0300

    initialize note-taking application with main and note modules

---
+++ Project3/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"note-app/note"
+	"os"
+	"strings"
+)
+
+func main() {
+	title, content := getNoteData()
+
+	userNote, err := note.New(title, content)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	userNote.Display()
+}
+
+func getNoteData() (string, string) {
+	title := getNoteInput("Note title: ")
+	content := getNoteInput("Note content: ")
+
+	return title, content
+}
+
+func getNoteInput(prompt string) string {
+	fmt.Print(prompt)
+
+	reader := bufio.NewReader(os.Stdin)
+	text, err := reader.ReadString('\n')
+
+	if err != nil {
+		return ""
+	}
+
+	text = strings.TrimSuffix(text, "\n")
+	text = strings.TrimSuffix(text, "\r")
+
+	return text
+}
