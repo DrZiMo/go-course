6918a41add08ea6caf0d04f3f426950bce33f493
DrZiMo <zuhaybhamar@gmail.com>
2026-03-04 18:07:47 +0300

    add initial implementation of concurrency with greeting functions

---
+++ concorruncy/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func greet(phrase string, doneChan chan bool) {
+	fmt.Println("Hello!", phrase)
+	doneChan <- true
+}
+
+func slowGreeting(phrase string, doneChan chan bool) {
+	time.Sleep(3 * time.Second)
+	fmt.Println("Hello!", phrase)
+	doneChan <- true
+}
+
+func main() {
+	done := make(chan bool)
+
+	go greet("Nice to meet you!", done)
+	go greet("How are you?", done)
+	go slowGreeting("How ... are ... you?", done)
+	go greet("I kinda liking this course", done)
+
+	<-done
+	<-done
+	<-done
+	<-done
+}
