c00beff6d01238861c19743d258649489a7dd916
DrZiMo <zuhaybhamar@gmail.com>
2026-03-04 12:27:39 +0300

    implement LoadData method for TaxIncludedPriceJob and update Processed logic

--- Price calculator/main.go
+++ Price calculator/main.go
@@ -1,19 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"price-calculator/price"
 )
 
 func main() {
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 
-	result := make(map[float64][]float64)
-
 	for _, taxRate := range taxRates {
 		priceJob := price.NewTaxIncludedPriceJob(taxRate)
 		priceJob.Processed()
 	}
-
-	fmt.Println(result)
 }
--- Price calculator/price/price.go
+++ Price calculator/price/price.go
@@ -1,6 +1,11 @@
 package price
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 type TaxIncludedPriceJob struct {
 	TaxRate             float64
@@ -8,11 +13,57 @@ type TaxIncludedPriceJob struct {
 	TaxIncludedPriceJob map[string]float64
 }
 
-func (job TaxIncludedPriceJob) Processed() {
-	result := make(map[string]float64)
+func (job *TaxIncludedPriceJob) LoadData() {
+	file, err := os.Open("data/prices.txt")
+
+	if err != nil {
+		fmt.Println("Could not open file!")
+		fmt.Println(err)
+		return
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	lines := []string{}
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	err = scanner.Err()
+
+	if err != nil {
+		fmt.Println("Reading file content failed!")
+		fmt.Println(err)
+		file.Close()
+		return
+	}
+
+	prices := make([]float64, len(lines))
+
+	for lineIndex, line := range lines {
+		floatPrice, err := strconv.ParseFloat(line, 64)
+
+		if err != nil {
+			fmt.Println("Failed to convert values to float")
+			fmt.Println(err)
+			file.Close()
+			return
+		}
+
+		prices[lineIndex] = floatPrice
+	}
+
+	job.InputPrices = prices
+}
+
+func (job *TaxIncludedPriceJob) Processed() {
+	job.LoadData()
+
+	result := make(map[string]string)
 
 	for _, price := range job.InputPrices {
-		result[fmt.Sprintf("%.2f", price)] = price * (1 + job.TaxRate)
+		taxIncludedPrice := price * (1 + job.TaxRate)
+		result[fmt.Sprintf("%.2f", price)] = fmt.Sprintf("%.2f", taxIncludedPrice)
 	}
 
 	fmt.Println(result)
