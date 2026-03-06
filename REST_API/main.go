511861af36b94d6485fc46507825d3ec238dacb9
DrZiMo <zuhaybhamar@gmail.com>
2026-03-06 15:54:33 +0300

    add initial implementation of REST API with event handling

---
+++ REST_API/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"net/http"
+	"rest_api/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+
+	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
+
+	server.Run(":8080") // localhost:8080
+}
+
+func getEvents(context *gin.Context) {
+	events := models.GetAllEvents()
+	context.JSON(http.StatusAccepted, gin.H{
+		"ok":     true,
+		"events": events,
+	})
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"ok":      false,
+			"message": err,
+		})
+		return
+	}
+	event.ID = 1
+	event.UserID = 1
+
+	context.JSON(http.StatusAccepted, gin.H{
+		"ok":      true,
+		"message": "Created event successfully",
+		"event":   event,
+	})
+}
